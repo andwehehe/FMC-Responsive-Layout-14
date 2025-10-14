718fe25d6c8677b6071663a3f08941edb97df9d3
andwe <andreigobres3@gmail.com>
2025-10-14 21:29:35 +0800

    completed theme switching

---
+++ main.js
@@ -0,0 +1,119 @@
+const themes = document.querySelectorAll("input[type='radio']");
+
+themes.forEach(theme => {
+  theme.addEventListener('change', () => {
+    if(themes[0].checked) {
+      defaultTheme();
+    } else if(themes[1].checked) {
+      secondTheme();
+    } else if(themes[2].checked) {
+      thirdTheme();
+    }
+  });
+})
+
+const body = document.querySelector("body");
+const heading = document.querySelector(".heading");
+const toggleContainer = document.querySelector(".toggle__container");
+const toggleBall = document.querySelector(".toggle__ball");
+const screen = document.querySelector(".screen");
+const keypad = document.querySelector(".keys__container");
+const keys = document.querySelectorAll(".keys");
+const del = document.querySelector(".del");
+const reset = document.querySelector(".reset");
+const equal = document.querySelector(".equal");
+
+function defaultTheme() {
+  body.classList.remove("body-2", "body-3");
+  heading.classList.remove("heading-2", "heading-3");
+  toggleContainer.classList.remove("toggle__container-2", "toggle__container-3");
+  toggleBall.classList.remove("toggle__ball-2", "toggle__ball-3");
+  screen.classList.remove("screen-2", "screen-3");
+  keypad.classList.remove("keys__container-2", "keys__container-3");
+  del.classList.remove("del-2", "del-3");
+  reset.classList.remove("reset-2", "reset-3");
+  equal.classList.remove("equal-2", "equal-3");
+
+  keys.forEach(key => {
+    key.classList.remove("keys-2", "keys-3");
+  });
+}
+
+function secondTheme() {
+  defaultTheme();
+
+  body.classList.add("body-2");
+  heading.classList.add("heading-2");
+  toggleContainer.classList.add("toggle__container-2");
+  toggleBall.classList.add("toggle__ball-2");
+  screen.classList.add("screen-2");
+  keypad.classList.add("keys__container-2");
+  del.classList.add("del-2");
+  reset.classList.add("reset-2");
+  equal.classList.add("equal-2");
+
+  keys.forEach(key => {
+    key.classList.add("keys-2");
+  });
+}
+
+function thirdTheme() {
+  defaultTheme();
+
+  body.classList.add("body-3");
+  heading.classList.add("heading-3");
+  toggleContainer.classList.add("toggle__container-3");
+  toggleBall.classList.add("toggle__ball-3");
+  screen.classList.add("screen-3");
+  keypad.classList.add("keys__container-3");
+  del.classList.add("del-3");
+  reset.classList.add("reset-3");
+  equal.classList.add("equal-3");
+
+  keys.forEach(key => {
+    key.classList.add("keys-3");
+  });
+}
+
+keys.forEach(key => {
+  key.addEventListener('click', () => {
+    const value = key.dataset.value;
+    const action = key.dataset.action;
+
+    if(action === "reset") {
+      screen.value = "";
+    } else if(action === "delete") {
+      screen.value = screen.value.slice(0, screen.value.length - 1);
+    } else if(action === "equals") {
+      equalCalc();
+    } else {
+      appendCalc(value);
+    }
+  })
+})     
+
+function equalCalc() {
+  const operators = ["+",  "-", "*", "/"];
+  let doesInclude = operators.some(operator => screen.value.endsWith(operator));
+  if(doesInclude || screen.value === "") {
+    return;
+  } else if(screen.value.includes("/0")) {
+    screen.value = "undefine";
+    return;
+  } else {
+    screen.value = eval(screen.value);
+  }
+}
+
+function appendCalc(value) {
+  const operators = ["+", "*", "/", "--"];
+  let doesInclude = operators.some(operator => screen.value.endsWith(operator));
+
+  if(screen.value === "" && (value === "+" || value === "/" || value === "*")) {
+    return;
+  } else if(doesInclude ) {
+    screen.value = screen.value.slice(0, - 1) + value;
+  } else {
+    screen.value += value;
+  }
+}
